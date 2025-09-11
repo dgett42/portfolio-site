cf19d3f2e90ca4a5dca48f451e1693765a7908d1
Donovan <degeter1@usf.edu>
2025-09-10 20:15:57 -0400

    Initial commit

---
+++ order-manager/cc_3b.js
@@ -0,0 +1,6 @@
+let inventory = [
+    { sku: "sku-001", name: "Hammer", price: 15.0, stock: 100 },
+    { sku: "sku-002", name: "Nails (100)", price: 5.0, stock: 200 },
+    { sku: "sku-003", name: "Screwdriver", price: 10.0, stock: 150 },
+    { sku: "sku-004", name: "Wrench", price: 12.0, stock: 80 },
+]
