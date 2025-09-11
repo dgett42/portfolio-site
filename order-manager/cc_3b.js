411bf5520a633a430d4a934aa866dbb0c27a6099
Donovan <degeter1@usf.edu>
2025-09-10 20:31:45 -0400

    second commit

--- order-manager/cc_3b.js
+++ order-manager/cc_3b.js
@@ -3,4 +3,6 @@ let inventory = [
     { sku: "sku-002", name: "Nails (100)", price: 5.0, stock: 200 },
     { sku: "sku-003", name: "Screwdriver", price: 10.0, stock: 150 },
     { sku: "sku-004", name: "Wrench", price: 12.0, stock: 80 },
-]
+];
+
+inventory.forEach(obj => console.log(`${obj.sku}\t|\t${obj.name}`));       
