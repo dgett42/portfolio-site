b65f54d38546d144c8205251093d091c426837a1
Donovan <degeter1@usf.edu>
2025-09-10 20:57:15 -0400

    Last Commit

--- order-manager/cc_3b.js
+++ order-manager/cc_3b.js
@@ -8,6 +8,6 @@ let inventory = [
 inventory.forEach(obj => console.log(`${obj.sku}\t|\t${obj.name}`));       
 
 // Step 3 
-inventory.shift({ sku: "sku-005", name: "Pliers", price: 8.0, stock: 120 });
+inventory.push({ sku: "sku-005", name: "Pliers", price: 8.0, stock: 120 });
 inventory.pop([1]);
-inventory.forEach(obj => console.log(`${obj.sku}\t|\t${obj.name}`));
+
