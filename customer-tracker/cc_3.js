04867061f59beb36be885be434439f6284de0f3d
dgett42 <degetter1@usf.edu>
2025-09-14 19:24:49 -0400

    Step 3

--- customer-tracker/cc_3.js
+++ customer-tracker/cc_3.js
@@ -2,4 +2,14 @@ let customers = [
     {name: "John", email: "Johndoe23@gmail.com", purchases: ["Bannanas, apples, oranges"]},
     {name: "Sarah", email: "sarahJane@gmail.com", purchases: ["pasta, rice, carrots"]},
     {name: "Malik", email: "maliktheman@gmail.com", purchases: ["pizza, Oreos, chicken"]}
-]
+] 
+
+console.log(customers)
+
+customers.push({Name: "sophia", emial: "sophie22@usf.edu", Purchases: ["flour, sugar, baking powder"]}) 
+
+console.log(customers)
+
+customers.shift([0])
+
+console.log(customers)
