a810f3c840b9f5f8a110cc965e0f1a95335b252d
dgett42 <degetter1@usf.edu>
2025-09-14 19:46:28 -0400

    Step 5

--- customer-tracker/cc_3.js
+++ customer-tracker/cc_3.js
@@ -1,18 +1,18 @@
 let customers = [
-    {name: "John", email: "Johndoe23@gmail.com", purchases: ["Bannanas, apples, oranges"]},
-    {name: "Sarah", email: "sarahJane@gmail.com", purchases: ["pasta, rice, carrots"]},
-    {name: "Malik", email: "maliktheman@gmail.com", purchases: ["pizza, Oreos, chicken"]}
+    {name: "John", email: "Johndoe23@gmail.com", purchases: ["Bannanas", "apples", "oranges"]},
+    {name: "Sarah", email: "sarahJane@gmail.com", purchases: ["pasta", "rice", "carrots"]},
+    {name: "Malik", email: "maliktheman@gmail.com", purchases: ["pizza", "Oreos", "chicken"]}
 ] 
 
 console.log(customers)
 
 // step 3
 
-customers.push({Name: "sophia", emial: "sophie22@usf.edu", Purchases: ["flour, sugar, baking powder"]}) 
+customers.push({name: "sophia", email: "sophie22@usf.edu", purchases: ["flour", "sugar", "baking powder"]}) 
 
 console.log(customers)
 
-customers.shift([0])
+customers.shift()
 
 console.log(customers)
 
@@ -20,3 +20,6 @@ console.log(customers)
 customers[1].purchases.push("Ketchup");
 
 console.log(customers);
+
+// Step 5 
+customers.forEach(customer => {console.log(`Name: ${customer.name}, Email: ${customer.email}, Total Purchases: ${customer.purchases.length}`)})
