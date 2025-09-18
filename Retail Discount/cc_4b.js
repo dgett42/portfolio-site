d9c5c87ac6414311027f397f6cda0c6fd48014ba
Donovan <degeter1@usf.edu>
2025-09-17 20:44:27 -0400

    Still working on step 4

--- Retail Discount/cc_4b.js
+++ Retail Discount/cc_4b.js
@@ -28,3 +28,15 @@ switch (product.category) {
 
 }
 console.log("After discounts:", products);
+
+// Step 4 
+
+let customerType = "student", ;
+let subtotal = 230;
+let discount = 0;
+
+if (customerType === "student") {
+    discount = 0.05; // 5% discount
+} else {
+    discount = 0;
+}
