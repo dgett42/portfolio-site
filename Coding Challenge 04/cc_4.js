b7a993c89602c06d329ff5e3ec3dcc19dd992d9c
Donovan <degeter1@usf.edu>
2025-09-21 19:05:48 -0400

    Step 2

---
+++ Coding Challenge 04/cc_4.js
@@ -0,0 +1,8 @@
+// Step 1: Create an array of product 
+let products = [
+    { name: "Laptop", category: "Electronics", price: 999.99, inventory: 10 },
+    { name: "Smartphone", category: "Electronics", price: 699.99, inventory: 25 },
+    { name: "Desk Chair", category: "household", price: 89.99, inventory: 15 },
+    { name: "Bookcase", category: "household", price: 129.99, inventory: 5 },
+    { name: "Shirts", category: "apparel", price: 19.99, inventory: 130 },
+]; 
