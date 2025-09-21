793a968a53a04b45b699140e9ac4dcb1064a6073
Donovan <degeter1@usf.edu>
2025-09-21 19:13:32 -0400

    Step 3

--- Coding Challenge 04/cc_4.js
+++ Coding Challenge 04/cc_4.js
@@ -1,8 +1,29 @@
 // Step 1: Create an array of product 
 let products = [
-    { name: "Laptop", category: "Electronics", price: 999.99, inventory: 10 },
-    { name: "Smartphone", category: "Electronics", price: 699.99, inventory: 25 },
+    { name: "Laptop", category: "electronics", price: 999.99, inventory: 10 },
+    { name: "Smartphone", category: "electronics", price: 699.99, inventory: 25 },
     { name: "Desk Chair", category: "household", price: 89.99, inventory: 15 },
     { name: "Bookcase", category: "household", price: 129.99, inventory: 5 },
     { name: "Shirts", category: "apparel", price: 19.99, inventory: 130 },
 ]; 
+
+for (let product of products) {
+    let discountRate = 0;
+
+switch (product.category) {
+    case "electronics":
+       discountRate = 0.20; // 20% discount
+        break;
+    case "apparel":
+        discountRate = 0.15; // 15% discount
+        break;
+    case "household":
+        discountRate = 0.1; // 10% discount
+        break;
+    default:
+        discountRate = 0;
+} 
+    let promoPrice = product.price * (1-discountRate);
+    console.log(`The promotional price for the ${product.name} is $${promoPrice.toFixed(2)}`);
+
+}
