4ee9fea71f1b1ae7855307a6ca637a28a3f42e45
Donovan <degeter1@usf.edu>
2025-09-21 20:01:53 -0400

    Step 4

--- Coding Challenge 04/cc_4.js
+++ Coding Challenge 04/cc_4.js
@@ -1,4 +1,4 @@
-// Step 1: Create an array of product 
+// Step 2: Create an array of products 
 let products = [
     { name: "Laptop", category: "electronics", price: 999.99, inventory: 10 },
     { name: "Smartphone", category: "electronics", price: 699.99, inventory: 25 },
@@ -6,6 +6,8 @@ let products = [
     { name: "Bookcase", category: "household", price: 129.99, inventory: 5 },
     { name: "Shirts", category: "apparel", price: 19.99, inventory: 130 },
 ]; 
+
+// Step 3: loops through the array and applies discounts based on category
 
 for (let product of products) {
     let discountRate = 0;
@@ -27,3 +29,24 @@ switch (product.category) {
     console.log(`The promotional price for the ${product.name} is $${promoPrice.toFixed(2)}`);
 
 }
+
+// Step 4: Create Variable for customer type and subtotal, then apply additional discounts
+
+let customerType = "student"; ;
+let subtotal = 230;
+let discount = 0;
+
+if (customerType === "student") {
+    discount = 0.05; // 5% discount
+} else if (customerType === "senior") {
+    discount = 0.07; // 7% discount
+} else {
+    discount = 0;     // No discount
+}
+
+let total = subtotal * (1 - discount);
+console.log(`Subtotal before discount: $${subtotal.toFixed(2)}`);
+console.log(`The total after a ${discount*100}% discount for a ${customerType} is $${total.toFixed(2)}`);
+
+
+
