c4f38cfade96222b7fc6fb51a80d5ef7fb8d2cbb
Donovan <degeter1@usf.edu>
2025-09-28 19:03:06 -0400

    Step 4

--- Payroll Processing Toolkit/cc_5.js
+++ Payroll Processing Toolkit/cc_5.js
@@ -13,3 +13,13 @@ let employeeObjects = [
 function calculateBasePay(rate, hours) {
     return rate * Math.min(hours, 40);
 }
+
+//Step 4
+
+function calculateOvertimePay(rate, hours) {
+    if (hours > 40) {
+        return (hours - 40) * rate * 1.5;
+    }
+    return 0;
+}
+
